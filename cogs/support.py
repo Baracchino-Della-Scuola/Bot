86b41b2b96265b5e20450bbf5cfa551e0235eaf3
cclauss <cclauss@users.noreply.github.com>
2022-01-13 12:58:08 +0000

    fixup! Format Python code with psf/black pull_request

--- cogs/support.py
+++ cogs/support.py
@@ -3,30 +3,41 @@ from discord.ext import commands
 
 
 class Counter(discord.ui.View):
-    @discord.ui.button(label='Open', style=discord.ButtonStyle.primary)
-    async def openticket(self, button: discord.ui.Button, interaction: discord.Interaction):
-        await interaction.repl('Opening', complete_hidden=True)
+    @discord.ui.button(label="Open", style=discord.ButtonStyle.primary)
+    async def openticket(
+        self, button: discord.ui.Button, interaction: discord.Interaction
+    ):
+        await interaction.repl("Opening", complete_hidden=True)
         member = interaction.author
         overwrites = {
-            interaction.guild.default_role: discord.PermissionOverwrite(read_messages=False),
-            member: discord.PermissionOverwrite(read_messages=True, send_messages=True)
+            interaction.guild.default_role: discord.PermissionOverwrite(
+                read_messages=False
+            ),
+            member: discord.PermissionOverwrite(read_messages=True, send_messages=True),
         }
-        await interaction.guild.create_text_channel('ticket-{}'.format(interaction.author), overwrites=overwrites)
-        await interaction.channel.send('Ticket opened in #ticket-{}'.format(interaction.author), complete_hidden=True)
-        
+        await interaction.guild.create_text_channel(
+            "ticket-{}".format(interaction.author), overwrites=overwrites
+        )
+        await interaction.channel.send(
+            "Ticket opened in #ticket-{}".format(interaction.author),
+            complete_hidden=True,
+        )
+
 
 class Support(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
-    @commands.command(name="support", aliases=["tickets", "ticket"], description="Open a support ticket")
+
+    @commands.command(
+        name="support",
+        aliases=["tickets", "ticket"],
+        description="Open a support ticket",
+    )
     @commands.is_owner()
     async def support(self, ctx):
         m = await ctx.send("Open a ticket here!")
         await m.add_reaction("📩")
 
 
-
-    
 def setup(bot):
     bot.add_cog(Support(bot))
--- main.py
+++ main.py
@@ -8,24 +8,27 @@ from pretty_help import DefaultMenu, PrettyHelp
 
 
 dotenv.load_dotenv(dotenv_path=".env")
+
+
 class Bot(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix=".", intents=discord.Intents.all(), slash_commands=True)
-        
+        super().__init__(
+            command_prefix=".", intents=discord.Intents.all(), slash_commands=True
+        )
+
         self.load_extension("jishaku")
-        menu = DefaultMenu(page_left="\U000025c0", page_right="\U000025b6",
-                           remove="\U0001f6d1", active_time=500, )
+        menu = DefaultMenu(
+            page_left="\U000025c0",
+            page_right="\U000025b6",
+            remove="\U0001f6d1",
+            active_time=500,
+        )
 
-
-# Custom ending note
+        # Custom ending note
         ending_note = f"(C) 2021 Il BaracchinoDella Scuola"
-
 
         self.help_command = PrettyHelp(menu=menu, ending_note=ending_note)
 
-        
-        
-        
     async def on_ready(self):
         for cog in os.listdir("./cogs"):
             if cog.endswith(".py"):
@@ -37,11 +40,23 @@ class Bot(commands.Bot):
                 except Exception as e:
                     print(f"Failed to load {cog}")
                     traceback.print_exc()
-        
+
         print("Bot is ready!")
         c = self.get_channel(907937553343209472)
         await c.send("Bot launched! Now you can start copying")
-        await self.change_presence(activity=discord.Game(name=random.choice(["Copy rush 2021", "Games at school", "Destroy the school", "Fake the test", "Copy Rush 2021 at school"])))
+        await self.change_presence(
+            activity=discord.Game(
+                name=random.choice(
+                    [
+                        "Copy rush 2021",
+                        "Games at school",
+                        "Destroy the school",
+                        "Fake the test",
+                        "Copy Rush 2021 at school",
+                    ]
+                )
+            )
+        )
 
 
 Bot().run(os.environ["token"])
